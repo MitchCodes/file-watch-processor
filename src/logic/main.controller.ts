96a57ac2833201010b7ed08e0768b58169849ea3
Mitch <mitchchiles@gmail.com>
2022-01-18 01:32:26 -0500

    Add retry logic for no controllers.

--- src/logic/main.controller.ts
+++ src/logic/main.controller.ts
@@ -18,23 +18,37 @@ export class MainController {
         this.cacheService = cacheService;
 
         let fileWatchControllerLoader: FileWatchControllerLoader = new FileWatchControllerLoader(logger, conf);
-        let fileWatchControllers: FileWatchController[] = await fileWatchControllerLoader.getFileWatchControllers();
+        let fileWatchControllers: FileWatchController[] = null;
 
-        if (!fileWatchControllers) {
-            this.logger.info('No file watch controllers. Stopping application.');
-            return;
+        let hasControllers: boolean = false;
+        while (!hasControllers) {
+            fileWatchControllers = await fileWatchControllerLoader.getFileWatchControllers();
+    
+            if (!fileWatchControllers) {
+                this.logger.info('No file watch controllers. Retrying in 60 seconds.');
+                await this.delay(60000);
+            } else {
+                hasControllers = true;
+            }
         }
 
         let fileWatchProcessor: FileWatchProcessor = new FileWatchProcessor(this.logger, this.conf);
         await fileWatchProcessor.processWatchControllers(fileWatchControllers);
-        
+
         // todo: Docker setup for linux handbrake. need to install handbrake using apt
         // Docker setup for mounting data configuration file
         // Docker setup for mounting watched folders
         // Docker setup documented in README
 
-        this.logger.info('Stopping application.');
+        // todo eventually: add functionality to hot-load new processors. have logic in processWatchControllers that will occasionally re-load the processors and then compare them for differences.
+        // try to reserve file lists if possible
+
+        this.logger.info('Stopping program.');
 
         return;
+    }
+
+    private delay(ms: number) {
+        return new Promise( resolve => setTimeout(resolve, ms) );
     }
 }
